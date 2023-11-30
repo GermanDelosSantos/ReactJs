3cfe6cf917ff4e4d9b839436344546230ac1476f
German De los Santos <noiconuf@gmail.com>
2023-11-30 20:08:19 -0300

    se modifico el componente Productos

--- src/components/productos/Productos.jsx
+++ src/components/productos/Productos.jsx
@@ -34,7 +34,7 @@ const products = [
     },
 ]
 
-export default function Example() {
+export function Productos() {
     return (
     <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -60,3 +60,5 @@ export default function Example() {
     </div>
     )
 }
+
+export default Productos
